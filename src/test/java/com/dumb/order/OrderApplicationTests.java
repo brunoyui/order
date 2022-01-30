792d659b8bc9b2dd69d07a95a86addbe71e06d4c
kusuri <kusuri@localhost.localdomain>
2022-01-30 00:42:39 -0200

    Initial commit project structure

---
+++ src/test/java/com/dumb/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumb.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
